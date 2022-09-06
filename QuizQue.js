1490996b9a3fbf9b717d99e3f44c80265a798f61
Arun-GK <112786320+Arun-GK@users.noreply.github.com>
2022-09-05 23:01:38 -0700

    Add files via upload

---
+++ QuizQue.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        question: "1st",
+        Option: ["A", "B", "C", "D"],
+        answer: 2,
+    },
+    {
+        question: "2rd",
+        Option: ["A", "B", "C", "D"],
+        answer: 2,
+    },
+    {
+        question: "3rd",
+        Option: ["A", "B", "C", "D"],
+        answer: 1,
+    },
+    {
+        question: "4th",
+        Option: ["A", "B", "C", "D"],
+        answer: 2,
+    },
+    {
+        question: "5th",
+        Option: ["A", "B", "C", "D"],
+        answer: 1,
+    },
+]
